f15d75e7ac9b1ad8375a3a9c9972837ab7f4864c
Adam Argyle <atom@argyleink.com>
2017-11-28 11:18:37 -0800

    we want to look in node modules for js deps

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ module.exports = {
   module: { rules: [
     // {
     //   test:     /\.js$/,
-    //   exclude:  /node_modules/,
     //   use:      [{
     //     loader: 'babel-loader',
     //     options: { presets: [
