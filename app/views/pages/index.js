11563ce5bb68a7c8a3cf3112fd7add2776f1097a
Adam Argyle <atom@argyleink.com>
2018-05-23 15:03:56 -0700

    replace bolt logo with handmade hex svg

--- app/views/pages/index.js
+++ app/views/pages/index.js
@@ -2,7 +2,7 @@ import 'ragrid'
 import 'CSS/elements/body.css'
 import 'CSS/elements/header.css'
 import 'CSS/elements/nav.css'
-import 'CSS/objects/bolt.css'
+import 'CSS/objects/hexagon.css'
 
 import { $ } from 'blingblingjs'
 
