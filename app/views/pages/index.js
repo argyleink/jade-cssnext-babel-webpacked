837cd156a314803aa94a2e7b492f34b9dbf19948
Adam Argyle <atom@argyleink.com>
2018-08-10 10:53:05 -0700

    update build to stable css chunks, update to latest blingbling

--- app/views/pages/index.js
+++ app/views/pages/index.js
@@ -3,6 +3,6 @@ import { typography } from '~/css/generic'
 import { body, header, nav } from '~/css/elements'
 import { hexagon } from '~/css/objects'
 
-import { $ } from 'blingblingjs'
+import $ from 'blingblingjs'
 
 console.log($('header'))
